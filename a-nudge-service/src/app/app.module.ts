8f357dae33fb59409987516f891ed18a146be78d
sanikashastri <42617115+sanikashastri@users.noreply.github.com>
2018-11-25 15:25:32 -0500

    Added angular app files

---
+++ a-nudge-service/src/app/app.module.ts
@@ -0,0 +1,74 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AddGroupComponent } from './components/add-group/add-group.component';
+
+import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
+
+import { AuthGuard } from './guards/auth.guard';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        DashboardComponent,
+        ProfileComponent,
+        AddGroupComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes, { useHash: true }),
+        FormsModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MatSnackBarModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatDialogModule,
+        MatCheckboxModule
+    ],
+    entryComponents: [AddGroupComponent],
+    providers: [ValidateService, AuthService, AuthGuard],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
