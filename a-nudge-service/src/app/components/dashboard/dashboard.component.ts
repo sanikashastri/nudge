4685322aadfbb5711a1dadd2ed21a780841bd4ce
sanikashastri <42617115+sanikashastri@users.noreply.github.com>
2018-12-26 12:47:56 -0500

    Added group functionalities

--- a-nudge-service/src/app/components/dashboard/dashboard.component.ts
+++ a-nudge-service/src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, group } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { ValidateService } from '../../services/validate.service';
 import { MatSnackBar } from '@angular/material';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { AddGroupComponent } from '../add-group/add-group.component';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material';
 
 @Component({
     selector: 'app-dashboard',
@@ -13,9 +16,16 @@ export class DashboardComponent implements OnInit {
     user: Object;
     group: Object;
     groups: any;
-    dialogBox: MatDialogRef<AddGroupComponent>;
+    dialogBox_addGroup: MatDialogRef<AddGroupComponent>;
 
-    constructor(private authService: AuthService,
+    visible = true;
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+    constructor(private validateService: ValidateService,
+                private authService: AuthService,
                 private snackBar: MatSnackBar,
                 private dialog: MatDialog) { }
 
@@ -24,7 +34,7 @@ export class DashboardComponent implements OnInit {
             this.user = profile.user;
 
             this.authService.getGroups(this.user["name"]).subscribe(groups => {
-                this.groups = groups.groups
+                this.groups = groups.groups;
             },
             err => {
                 console.log(err);
@@ -37,10 +47,38 @@ export class DashboardComponent implements OnInit {
         });
     }
 
-    onNewGroup() {
-        this.dialogBox = this.dialog.open(AddGroupComponent, {width: '25%'});
+    addMember(event: MatChipInputEvent, group) {
+        const input = event.input;
+        const value = event.value;
+        
+        if (!this.validateService.validateEmail(value)) {
+            this.snackBar.open('Please use a valid email', '', {duration: 3000});
+            return false;
+        }
 
-        this.dialogBox.afterClosed().subscribe(result => {
+        if ((value || '').trim()) {
+            group["members"].push(value.trim());
+        }
+
+        if (input) {
+            input.value = '';
+        }
+    }
+
+    removeMember(group, member): void {
+        const index = group["members"].indexOf(member);
+
+        if (index > 0) {
+            group["members"].splice(index, 1);
+        } else {
+            this.snackBar.open('Cannot remove admin', '', {duration: 3000});
+        }
+    }
+
+    onNewGroup() {
+        this.dialogBox_addGroup = this.dialog.open(AddGroupComponent, {width: '25%'});
+
+        this.dialogBox_addGroup.afterClosed().subscribe(result => {
             this.group = JSON.stringify(result);
 
             this.authService.createGroup(this.group).subscribe(data => {
@@ -51,6 +89,24 @@ export class DashboardComponent implements OnInit {
                     this.snackBar.open('Something went wrong', '', {duration: 3000});
                 }
             });
+        });
+    }
+
+    onUpdateSubmit(group) {
+        this.group = JSON.stringify(group);
+
+        if (!this.validateService.validateGroup(group)) {
+            this.snackBar.open('Please fill in all fields', '', {duration: 3000});
+            return false;
+        }
+
+        this.authService.updateGroup(this.group).subscribe(data => {
+            if (data.success) {
+                this.ngOnInit();
+                this.snackBar.open('Your group has been updated', '', {duration: 3000});
+            } else {
+                this.snackBar.open('Something went wrong', '', {duration: 3000});
+            }
         });
     }
 }
